7a577c33a2f7c8900167937633818ccc15ba21a8
thanushkrishna <jeevanthanush@gmail.com>
2023-01-17 22:26:49 +0530

    api created

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const axios = require("axios")
-const scrapeit = require("scrape-it")
-const cheerio = require("cheerio")
-axios.get("https://themeisle.com/blog/best-free-blogging-sites")
-.then(
-    (res)=>{
-        const $ = cheerio.load(res.data)
-        const data = scrapeit.scrapeHTML($,{scrapedData:{
-            listItem: "h3", data: {
-                createdAt: {
-                    selector: ".date"
-                  , convert: x => new Date(x)
-                }
-              , title: "article"
-              , tags: {
-                    listItem: "a"
-                }
-              , content: {                    
-                   how: "text"
-                }
-              , traverseOtherNode: {
-                    selector: ".upperNode"
-                  , closest: "div"
-                  , convert: x => x.length
-                }
-        }}})
-        console.log(data)
-        
-    })
-.catch((err)=>{console.log(err)})
+const express = require('express');
+const scrape = require('scrape-it');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-console.log("Hello")
+const app = express();
+
+app.use(express.json());
+
+app.listen(8080, (err)=> {
+    console.log("App Started");
+})
+.on('error', (err)=>{
+    console.log(err)
+})
+
+app.get('/webscrape', (req, res) => {
+    res.status(200).send( "<h1> Welcome to Web Scrapper</h1>" )
+})
+
+// axios.get("https://themeisle.com/blog/best-free-blogging-sites")
+// .then( 
+//     (res)=>{
+//         console.log(res);
+//     }
+// )
+// .catch(
+//     (err)=>{
+//         console.log(err);
+
+// })
